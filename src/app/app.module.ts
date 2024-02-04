69e004929ce1685c5f59e11f243c2d89e54b63f2
Daniel Martinez <roanhawkeye@gmail.com>
2024-02-04 11:43:58 -0600

    [task-3] - Add interceptor with mock token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { AuthInterceptor } from './shared/helpers/auth.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
     SharedModule,
     PokemonModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -3,15 +3,21 @@ import { Injectable } from '@angular/core';
 @Injectable({providedIn: 'root'})
 export class AuthService {
   public isUserLogin: boolean = false;
+  public authToken: string = '';
 
   constructor() { }
 
   login(){
     this.isUserLogin = true;
+    this.generateToken();
   }
 
   logout(){
     this.isUserLogin = false;
+  }
+
+  private generateToken(){
+    this.authToken = 'jwtTokenMock';
   }
 
 
